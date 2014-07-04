2db97b28ba51502f63fd1c64eeebe18a0a06c9ea
Patric Schenke <booiiing@gmail.com>
2014-07-04 15:03:05 +0200

    add robospice-request to get an oauth2-accessgrant using refresh-token

---
+++ src/main/java/tv/px/android/robospice/robospice_oauth2/request/OAuth2RefreshAccessRequest.java
@@ -0,0 +1,34 @@
+package tv.px.android.robospice.robospice_oauth2.request;
+
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.OAuth2Template;
+
+/**
+ * Get a new {@link AccessGrant} by refreshing the token.
+ * 
+ */
+public class OAuth2RefreshAccessRequest extends OAuth2AccessRequest {
+
+   private String refreshToken;
+
+   /**
+    * Initialize the request.
+    * 
+    * @param clazz
+    *           class of the expected result
+    * @param _oauth2Template
+    *           initialized template to perform the communication
+    * @param _refreshToken
+    *           refresh token used to authenticate
+    */
+   public OAuth2RefreshAccessRequest(Class<AccessGrant> clazz, OAuth2Template _oauth2Template, String _refreshToken) {
+      super(clazz, _oauth2Template);
+      this.refreshToken = _refreshToken;
+   }
+
+   @Override
+   public AccessGrant loadDataFromNetwork() throws Exception {
+      return this.oauth2Template.refreshAccess(refreshToken, null);
+   }
+
+}
