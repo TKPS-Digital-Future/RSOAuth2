cf23438756fcfaf34573c97f9f221775ec634f6a
Patric Schenke <booiiing@gmail.com>
2014-07-04 15:02:08 +0200

    add abstract base-class for requests that produce an oauth2-accessgrant

---
+++ src/main/java/tv/px/android/robospice/robospice_oauth2/request/OAuth2AccessRequest.java
@@ -0,0 +1,28 @@
+package tv.px.android.robospice.robospice_oauth2.request;
+
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.OAuth2Template;
+
+import com.octo.android.robospice.request.SpiceRequest;
+
+/**
+ * Superclass for requests to gain an OAuth2 {@link AccessGrant}.
+ * 
+ */
+public abstract class OAuth2AccessRequest extends SpiceRequest<AccessGrant> {
+
+   protected OAuth2Template oauth2Template;
+
+   /**
+    * Default constructor sets the {@link OAuth2Template}.
+    * 
+    * @param clazz
+    *           the class of the expected result
+    * @param _oauth2Template
+    *           an initialized OAuth2Template
+    */
+   public OAuth2AccessRequest(Class<AccessGrant> clazz, OAuth2Template _oauth2Template) {
+      super(clazz);
+      this.oauth2Template = _oauth2Template;
+   }
+}
