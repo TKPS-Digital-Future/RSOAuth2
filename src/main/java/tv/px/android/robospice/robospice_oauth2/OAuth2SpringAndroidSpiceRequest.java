47266e3b01d44622b2010770cbac8ee57d15f17f
Patric Schenke <booiiing@gmail.com>
2014-07-04 14:22:17 +0200

    add skeleton-class for SpiceRequest-sublass

---
+++ src/main/java/tv/px/android/robospice/robospice_oauth2/OAuth2SpringAndroidSpiceRequest.java
@@ -0,0 +1,39 @@
+package tv.px.android.robospice.robospice_oauth2;
+
+import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
+
+/**
+ * Abstract class for requests that try to access resources protected by OAuth2. Sets the HTTP-Authentication-header. If
+ * you set a {@link OAuth2RetryPolicy} as retry-policy, this becomes even more useful.
+ * 
+ * @param <RESULT>
+ *           type of the expected result
+ */
+public abstract class OAuth2SpringAndroidSpiceRequest<RESULT> extends SpringAndroidSpiceRequest<RESULT> {
+
+   /**
+    * Default constructor.
+    * 
+    * @param clazz
+    *           the class-representation of the expected result
+    */
+   public OAuth2SpringAndroidSpiceRequest(Class<RESULT> clazz) {
+      super(clazz);
+   }
+
+   /**
+    * Where the actual work is done. Adds the token to the HTTP-headers before sending the request.
+    * 
+    * @see com.octo.android.robospice.request.SpiceRequest#loadDataFromNetwork()
+    * 
+    * @return the result of the request
+    */
+   @Override
+   public RESULT loadDataFromNetwork() {
+      // TODO add OAuth2-authorization-header to request
+      // TODO fire request
+      // TODO handle generic exceptions
+      return null;
+   }
+
+}
